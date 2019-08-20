5d645f9505e3ee470db9fa4e3ec097bed3deb2de
qian.wq <qian.wq@dtwave-inc.com>
2019-08-20 21:52:08 +0800

    feat: 场景管理无场景占位

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -1,4 +1,4 @@
-import {Component} from 'react'
+import {Component, Fragment} from 'react'
 import {action, toJS} from 'mobx'
 import {observer, inject} from 'mobx-react'
 import {
@@ -8,6 +8,7 @@ import {Link} from 'react-router-dom'
 
 import {Time} from '../common/util'
 import {navListMap} from '../common/constants'
+import NoData from '../component-scene-nodata'
 import ModalAdd from './modal-add'
 
 import Del from '../svg-component/Del'
@@ -50,82 +51,85 @@ export default class Scene extends Component {
 
   render() {
     const {loading, list = []} = store
+
     return (
-      <div className="scene-wrap p16 pt0">
+      <div className="scene-wrap">
         <div className="header">标签使用场景</div>
-        <Row gutter={16} className="scene-box"> 
-          <Spin spinning={loading}>
-            <Col span={8}>
-              <div className="add-scene FBH FBJC FBAC">
-                <div className="pt16" onClick={() => this.handleModalVisible()}>
-                  <div className="add-icon">
-                    <Icon type="plus" />
-                  </div>
-                  <p className="mt12">添加场景</p>
-                </div>
-              </div>
-            </Col>
+        <Spin spinning={loading}>
+          <div className="scene-box">
+          
             {
-              toJS(list).map(({
-                id,
-                name,
-                cUser,
-                cDate,
-                used,
-                tagCount,
-                apiCount,
-                descr,
-              }, d) => (
-                <Col span={8}>
-                  <div className={`card ${used ? 'used' : 'noused'}`}>
-                    <div className="item-info">
-                      <div className="c-name">
-                        <Link to={`/detail/${id}`} title={name} className="mr8 omit">{name}</Link>
-                        <Tag color={used ? 'green' : 'blue'}>{used ? '使用中' : '未使用'}</Tag>
-                        <Icon type="right" />
-                      </div>
-                      <div className="c-info">
-                        <span className="mr20">
+              list.length ? (
+                <Fragment>
+                  <Button className="mb16" type="primary" onClick={() => this.handleModalVisible()}>添加场景</Button>
+                  <Row gutter={16}> 
+           
+                    {
+                      list.map(({
+                        id,
+                        name,
+                        cUser,
+                        cDate,
+                        used,
+                        tagCount,
+                        apiCount,
+                        descr,
+                      }, d) => (
+                        <Col span={8}>
+                          <div className="card">
+                            <div className="item-info">
+                              <div className="c-name">
+                                <Link to={`/detail/${id}`} className={`name ${used ? 'use-name' : ''}`} title={name}>{name}</Link>
+                                <Tag color={used ? 'blue' : ''}>{used ? '使用中' : '未使用'}</Tag>
+                                <Icon type="right" />
+                              </div>
+                              <div className="c-info">
+                                <span className="mr20">
                           创建者：
-                          {cUser}
-                        </span> 
-                        <span>
+                                  {cUser}
+                                </span> 
+                                <span>
                           创建时间：
-                          <Time timestamp={cDate} />
-                        </span>
-                      </div>
-                      <div className="c-descr omit" title={descr}>
-                        描述：
-                        {descr}
-                      </div>
-                      <div className="count-info">
-                        <div>
-                          <span>标签数</span>
-                          <div className="count">{tagCount}</div>
-                        </div>
-                        <div className="line" />
-                        <div>
-                          <span>API数</span>
-                          <div className="count">{apiCount}</div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="item-tool">
-                      <Button type="link" disabled={used} className="tool" onClick={() => this.handleModalVisible('edit', list[d])}>
-                        <Edit size="14" className={used ? 'i-used' : 'i-btn'} />
-                      </Button>
-                      <div className="line" />
-                      <Button type="link" disabled={used} className="tool" onClick={() => this.handleDel(id)}>
-                        <Del size="14" className={used ? 'i-used' : 'i-btn'} />
-                      </Button>
-                    </div>
-                  </div>
-                </Col>
-              ))
+                                  <Time timestamp={cDate} />
+                                </span>
+                              </div>
+                              <div className="c-descr omit" title={descr}>
+                          描述：
+                                {descr}
+                              </div>
+                              <div className="count-info">
+                                <div>
+                                  <span>标签数</span>
+                                  <div className="count">{tagCount}</div>
+                                </div>
+                                <div className="line" />
+                                <div>
+                                  <span>API数</span>
+                                  <div className="count">{apiCount}</div>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="item-tool">
+                              <Button type="link" disabled={used} className="tool" onClick={() => this.handleModalVisible('edit', list[d])}>
+                                <Edit size="14" className={used ? 'i-used' : 'i-btn'} />
+                              </Button>
+                              <div className="line" />
+                              <Button type="link" disabled={used} className="tool" onClick={() => this.handleDel(id)}>
+                                <Del size="14" className={used ? 'i-used' : 'i-btn'} />
+                              </Button>
+                            </div>
+                          </div>
+                        </Col>
+                      )) 
+                    }
+           
+                  </Row>
+                </Fragment>
+              ) : <NoData btnTxt="添加场景" onClick={() => this.handleModalVisible()} />
             }
-          </Spin>
-        </Row>
-        <ModalAdd store={store} />
+            <ModalAdd store={store} />
+          </div>
+        </Spin>
       </div>
     )
   }
