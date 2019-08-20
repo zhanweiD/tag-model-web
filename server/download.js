0c3405ff0e1e61531525e91d72277bed949cca7b
qiong.wuq <qiong.wq@dtwave-inc.com>
2019-08-20 12:58:02 +0800

    commit

--- server/download.js
+++ server/download.js
@@ -3,14 +3,15 @@ const forward = require('koa-forward-request2')
 // 临时目录
 function download(app) {
   const {router, config} = app
+  const pathPrefix = config('server.pathPrefix')
 
   forward(app, {
     debug: true,
   })
   // 提供下载中转
-  router.all('/file/download/*', async ctx => {
-    const url = ctx.url.replace('/file/download', '')
-    ctx.forward(`${config('server.fs.apiPrefix')}${url}`)
+  router.all(`${pathPrefix}/file/download/*`, async ctx => {
+    const url = ctx.url.replace(`${pathPrefix}/file/download`, '')
+    ctx.forward(`${config('server.apiPrefix')}${url}`)
   })
 }
 module.exports = download
