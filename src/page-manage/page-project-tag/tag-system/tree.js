fae1613d0c9d2ed509410003b551401fb97dccfc
zhanwei <@zhanwei.dzw>
2020-08-04 16:29:11 +0800

    标签体系重置key

--- src/page-manage/page-project-tag/tag-system/tree.js
+++ src/page-manage/page-project-tag/tag-system/tree.js
@@ -17,6 +17,7 @@ export default class Tree extends Component {
     this.store = props.store
 
     this.store.projectId = props.projectId
+    this.store.selectedKey = null
     this.store.commonTag = props.commonTag
     this.store.getTreeData()
   }
