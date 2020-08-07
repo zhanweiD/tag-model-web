c64fa10e31681b9361392caf845c478a43ebb590
qian.wq <qian.wq@dtwave-inc.com>
2020-08-07 17:20:49 +0800

    feat: 权限

--- src/page-common/main.js
+++ src/page-common/main.js
@@ -5,8 +5,6 @@ import {useEffect} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
 import OnerFrame from '@dtwave/oner-frame' 
 
-import ProjectConfig from './project-config'
-import ProjectList from './project-list'
 import Approval from './approval'
 
 const prePath = '/common'
@@ -21,11 +19,6 @@ export default () => {
   }, [])
   return (
     <Switch>
-      {/* 项目配置 */}
-      <Route exact path={`${prePath}/project/:projectId`} component={ProjectConfig} />
-      {/* 项目管理 */}
-      <Route exact path={`${prePath}/project`} component={ProjectList} />
-
       {/* 审批管理 */}
       <Route exact path={`${prePath}/approval/:type?`} component={Approval} />
       <Redirect strict to={`${prePath}/project`} />
