5688c15bb216333171d0689f65bebc085cd042dc
qian.wq <qian.wq@dtwave-inc.com>
2020-08-14 17:56:50 +0800

    fix: 项目描述

--- src/page-manage/page-tag-model/tag-detail/project-list.js
+++ src/page-manage/page-tag-model/tag-detail/project-list.js
@@ -15,7 +15,7 @@ export default class ProjectList extends Component {
     }, {
       title: '项目描述',
       key: 'descr',
-      dataIndex: 'descrs',
+      dataIndex: 'descr',
     }, {
       title: '申请时间',
       key: 'ctime',
