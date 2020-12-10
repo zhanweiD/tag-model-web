8f6f0f1a72305d3db94e75ad23ae3aa8c8d62799
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2020-12-10 11:38:01 +0800

    145

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: [
       {
         context: ['/config', '/api'],
-        target: 'http://192.168.90.159',
+        target: 'http://192.168.90.145',
         changeOrigin: true,
       },
     ],
