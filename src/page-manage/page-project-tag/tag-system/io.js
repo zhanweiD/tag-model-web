06fb7a9a82767a68bc431162f438ca1a20a926ec
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2020-12-18 17:40:45 +0800

    current更新

--- src/page-manage/page-project-tag/tag-system/io.js
+++ src/page-manage/page-project-tag/tag-system/io.js
@@ -7,7 +7,7 @@ const api = {
 
   getTagBaseDetail: get(`${tagModalApi}/tag_detail`), // 标签详情
   applyTag: post(`${marketApi}/tagApply`), // 标签申请
-  getProjectDetail: post('/api/project/1_0_0/project/detail'), // 项目详情
+  getProjectDetail: post('/api/project/current/project/detail'), // 项目详情
   getTreeData: get(`${baseApi}/tag/pool/tag_tree_system`), // 标签体系树
   getTreeDataPro: get(`${baseApi}/tag/pool/tag_tree_system_project`), // 标签体系树(项目下)
   // getTreeData: get('http://192.168.90.129:3000/mock/119/tag/pool/tag_tree_system'), // 标签体系树
